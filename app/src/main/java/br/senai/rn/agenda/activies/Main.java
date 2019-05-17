1b8a4efb9c4ea473b8a67d79d7b807a931b088f6
Aluno <aluno@fiern.local>
2019-05-17 14:24:45 -0300

    first commit

---
+++ app/src/main/java/br/senai/rn/agenda/activies/Main.java
@@ -0,0 +1,34 @@
+package br.rn.senai.br.agenda;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        setTitle("Lista de alunos");
+
+        List<String> alunos = new ArrayList<String>();
+        alunos.add("Paulo");
+        alunos.add("João");
+        alunos.add("Maria");
+
+        ListView listaAlunos = (ListView) findViewById(R.id._dynamic_main_list_alunos);
+
+        listaAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos
+        ));
+
+    }
+}
